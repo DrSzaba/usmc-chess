7964a12b469ee7a55e875f581420c6f0280aea38
Dr. Szaba <132481120+DrSzaba@users.noreply.github.com>
2026-09-25 16:10:55 -0400

    Make bishop mitre prominent and refresh 3D assets

--- dist/preview/collectors/presentation.js
+++ dist/preview/collectors/presentation.js
@@ -2,7 +2,7 @@ import * as T from 'three';
 import { OrbitControls } from '../../vendor/OrbitControls.js';
 import { RoomEnvironment } from '../../vendor/RoomEnvironment.js';
 import { Reflector } from '../../vendor/Reflector.js';
-import { makeWorkshop } from './sculptures.js';
+import { makeWorkshop } from './sculptures.js?v=mitre-3';
 
 const CELL=1.15;
 const coords=s=>[(s.charCodeAt(0)-97-3.5)*CELL,(8-Number(s[1])-3.5)*CELL];
